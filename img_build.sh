5836587d21bbb5172377ee9367ae9abf5d3452de
Shivam Rajesh Tiwari <69886545+shivamt002@users.noreply.github.com>
2023-12-08 03:10:06 +0000

    Update img_build.sh

--- img_build.sh
+++ img_build.sh
@@ -2,19 +2,19 @@
 
 echo "\n*************** Building the Image 1 ***************\n"
 
-docker build -t krishgopal37/image_1 ./enhance_docker_sec/target_1/
+docker build -t shivamtiwari38/csom/image_1 ./enhance_docker_sec/target_1/
 
 echo "\n*************** Building the Image 2 ***************\n"
 
-docker build -t krishgopal37/image_2 ./enhance_docker_sec/target_2/
+docker build -t shivamtiwari38/csom/image_2 ./enhance_docker_sec/target_2/
 
 echo "\n*************** Pushing the Image 1 to the repo ***************\n"
 
-docker push krishgopal37/image_1
+docker push shivamtiwari38/csom/image_1
 
 echo "\n*************** Pushing the Image 2 to the repo ***************\n"
 
-docker push krishgopal37/image_2
+docker push shivamtiwari38/csom/image_2
 
 echo "\n*************** Scanning the Images using trivy ***************\n"
 
