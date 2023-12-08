4499c22713f2dcef52631b262874728b5980661c
Shivam Rajesh Tiwari <69886545+shivamt002@users.noreply.github.com>
2023-12-08 02:10:48 +0000

    Update multiprocess.py
    Created a dir to store the scan results

--- multiprocess.py
+++ multiprocess.py
@@ -9,6 +9,7 @@ import sys
 def io_bound(image_name):
 
     logging.info("Entering image scanning function")
+    os.makedirs("scan_results", exist_ok = True)
 
 
     try:            
@@ -23,8 +24,9 @@ def io_bound(image_name):
 
         # scan_image = "trivy -q image -f table {}".format(image_name)
         # scan_result = subprocess.check_output(scan_image.split()).decode('utf-8')
+        directory_path = "scan_result"
         result_file = image_name.split("/")[1]      
-        subprocess.run(f"trivy image {image_name}:latest --timeout 30m > {result_file}.txt", shell=True)
+        subprocess.run(f"trivy image {image_name}:latest --timeout 30m > {directory_path}/{result_file}.txt", shell=True)
 
             
         print(f"{pid} * {processName} * {threadName} * {image_name}  \
