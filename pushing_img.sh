d0608ce874ea486e792882a236d1e37f4eaa49ec
Shivam Rajesh Tiwari <69886545+shivamt002@users.noreply.github.com>
2023-12-08 03:10:25 +0000

    Update pushing_img.sh

--- pushing_img.sh
+++ pushing_img.sh
@@ -1,9 +1,9 @@
 echo "\n*************** Pushing the Image 1 to the repo ***************\n"
 
-docker push krishgopal37/image_1
+docker push shivamtiwari38/csom/image_1
 
 echo "\n*************** Pushing the Image 2 to the repo ***************\n"
 
-docker push krishgopal37/image_2
+docker push shivamtiwari38/csom/image_2
 
 echo "\n*************** Done *********************\n"
