b81e72689365c4721cab60d5cfd9b331208f33fe
Shivam Rajesh Tiwari <69886545+shivamt002@users.noreply.github.com>
2023-12-10 00:28:46 +0000

    Update img_build.sh

--- img_build.sh
+++ img_build.sh
@@ -6,13 +6,21 @@ docker build -t shivamtiwari38/csom/image_1 ./enhance_docker_sec/target_1/
 
 echo "\n*************** Building the Image 2 ***************\n"
 
-docker build -t shivamtiwari38/csom/image_2 ./enhance_docker_sec/target_3/
+docker build -t shivamtiwari38/csom/image_2 ./enhance_docker_sec/target_2/
+
+echo "\n*************** Building the Image 3 ***************\n"
+
+docker build -t shivamtiwari38/csom/image_3 ./enhance_docker_sec/target_3/
 
 echo "\n*************** Pushing the Image 1 to the repo ***************\n"
 
 docker push shivamtiwari38/csom/image_1
 
 echo "\n*************** Pushing the Image 2 to the repo ***************\n"
+
+docker push shivamtiwari38/csom/image_2
+
+echo "\n*************** Pushing the Image 3 to the repo ***************\n"
 
 docker push shivamtiwari38/csom/image_3
 
